8a844159ed387b31f59967f788cdd0ed8bc7ec77
Thomas Leskin <tleskin@gmail.com>
2015-07-07 09:49:35 -0600

    Complete forEach

--- examples/01-forEach/script.js
+++ examples/01-forEach/script.js
@@ -6,3 +6,11 @@
 // There is a function defined for you called `addInstagramImageToThePage`,
 // which will take care of adding the image to the page. You just need to worry
 // about iterating over all of the images for this exercise.
+
+// photographs.forEach(function(photograph){
+//   addInstagramImageToThePage(photograph);
+// });
+
+// Shorter way
+// First argument that's passed is each photo
+photographs.forEach(addInstagramImageToThePage)
